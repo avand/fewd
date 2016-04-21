aeb264ec7e5ef6fa1daf6d8b4e712aab2dffb3e8
Avand Amiri <avand@avandamiri.com>
2016-04-21 12:11:06 -0700

    Adding resources from FEWD 11

---
+++ 11/Week 4/New Project Template v2/javascripts/application.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  // Your JS goes here...
+};
---
+++ 11/Week 8/each.js
@@ -0,0 +1,19 @@
+// Interface:
+var $ = {
+  each: function(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+      callback(i, array[i])
+    }
+  }
+};
+
+// Implementation:
+var colors = ["red", "green", "blue"]
+$.each(colors, function(i, color) {
+  console.log(i, color);
+})
+
+// Outputs:
+// 0, "red"
+// 1, "green"
+// 2, "blue"
