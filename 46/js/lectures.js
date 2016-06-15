688bdc795ae86b1316889ceaaccef257a946f3aa
Avand Amiri <avand@avandamiri.com>
2016-06-15 12:10:29 -0700

    Improving hello world (GitHub setup) instructions by adding screenshots

--- 46/js/lectures.js
+++ 46/js/lectures.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
   $("nav").load("../../partials/lecture_navigation.html");
+
+  var timer;
+  var skipEnlargment = false;
+
+  $(".enlarge-on-hover").mouseenter(function() {
+    if (skipEnlargment) return;
+
+    var img = $(this);
+    var rect = this.getBoundingClientRect();
+
+    timer = setTimeout(function() {
+      console.log("url(" + img.attr("src") + ")");
+      $("<div>").addClass("image-enlargement-target").css({
+        position: "fixed",
+        top: rect.top,
+        left: rect.left,
+        width: img.width(),
+        height: img.height()
+      }).mouseleave(function() {
+        $(this).remove();
+        console.log("remove")
+        img.removeAttr("style");
+        setTimeout(function() { skipEnlargment = false; }, 100);
+      }).appendTo("body")
+
+      img.css({
+        position: "fixed",
+        top: "20px",
+        left: "5%",
+        right: "5%",
+        width: "90%"
+      });
+
+      skipEnlargment = true;
+    }, 200);
+  }).mouseleave(function() {
+    clearTimeout(timer);
+  })
+
 })
