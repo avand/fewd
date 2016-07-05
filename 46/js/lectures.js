4f196f60895975dcb1288be9722d773311c1dcfc
Avand Amiri <avand@avandamiri.com>
2016-07-05 00:58:57 -0700

    Lots of JavaScript notes for week 3

--- 46/js/lectures.js
+++ 46/js/lectures.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("nav").load("../../partials/lecture_navigation.html");
 
+  var bootstrap = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css";
+  var bootstrapTheme = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css";
+  $("head").append("<link rel='stylesheet' href='" + bootstrap + "'>");
+  $("head").append("<link rel='stylesheet' href='" + bootstrapTheme + "'>");
+
   var timer;
   var skipEnlargment = false;
 
