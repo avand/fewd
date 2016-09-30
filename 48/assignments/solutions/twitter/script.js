7dfd63c8b15e6055eee1a4072ae529c8ce66a9c0
Avand Amiri <avand@avandamiri.com>
2016-09-30 01:29:08 -0700

    Twitter solution

--- 48/assignments/solutions/twitter/script.js
+++ 48/assignments/solutions/twitter/script.js
@@ -4,15 +4,50 @@ var likeLink = document.querySelector(".tweet-actions a")
 
 form.addEventListener("submit", formSubmitted);
 
-function formSubmitted(formSubmitEvent) {
-  console.log(formSubmitEvent);
+function createTweet(textContent, image) {
+  var li = document.createElement("li");
+  var avatar = document.createElement("div");
+  var img = document.createElement("img");
+  var text = document.createElement("div");
+  var textDiv = document.createElement("div");
+  var actions = document.createElement("div");
+  var link = document.createElement("a");
+  var span = document.createElement("span");
 
+  li.classList.add("tweet");
+  avatar.classList.add("tweet-avatar");
+  text.classList.add("tweet-text");
+  actions.classList.add("tweet-actions");
+  span.classList.add("like-count");
+
+  img.setAttribute("width", "64");
+  img.setAttribute("height", "64");
+  img.setAttribute("src", image);
+  link.setAttribute("href", "#");
+
+  textDiv.textContent = textContent;
+  link.textContent = "Like";
+  span.textContent = "(0)";
+
+  li.appendChild(avatar);
+  avatar.appendChild(img);
+  li.appendChild(text);
+  text.appendChild(textDiv);
+  text.appendChild(actions);
+  actions.appendChild(link);
+  actions.appendChild(span);
+
+
+  // document.querySelector(".tweets").appendChild(li);
+
+  var tweets = document.querySelector(".tweets")
+  tweets.insertBefore(li, tweets.firstChild);
+}
+
+function formSubmitted(formSubmitEvent) {
   formSubmitEvent.preventDefault();
 
-  var tweet = document.createElement("div");
-  tweet.textContent = textarea.value;
-
-  document.querySelector(".tweets").appendChild(tweet);
+  createTweet(textarea.value, "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg")
 }
 
 textarea.addEventListener("keydown", decrementCharCount);
@@ -30,4 +65,4 @@ function like(mouseEvent) {
   console.log(mouseEvent);
 }
 
-// someParentObject.insertBefore(someChildObject,someParentObject.firstChild);
+//
